b8487f9e86d0c347f924c5a9e1f42defd90c87be
vladimir <chebukin@artw.ru>
2023-12-25 15:53:22 +0300

    start

---
+++ cmd/storage/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"storage/internal/storage"
+)
+
+func main() {
+	st := storage.NewStorage()
+
+	file, err := st.Upload("test.txt", []byte("hello"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	restoredFile, err := st.GetById(file.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("it is restored", restoredFile)
+}
---
+++ internal/storage/storage.go
@@ -0,0 +1,37 @@
+package storage
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"storage/internal/file"
+)
+
+type Storage struct {
+	Files map[uuid.UUID]*file.File
+}
+
+func NewStorage() *Storage {
+	return &Storage{
+		Files: make(map[uuid.UUID]*file.File),
+	}
+}
+
+func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
+	file, err := file.NewFile(filename, blob)
+	if err != nil {
+		return nil, err
+	}
+
+	s.Files[file.ID] = file
+
+	return file, nil
+}
+
+func (s *Storage) GetById(fileID uuid.UUID) (*file.File, error) {
+	f, ok := s.Files[fileID]
+	if ok != true {
+		return nil, fmt.Errorf("file %v not found", fileID)
+	}
+
+	return f, nil
+}
